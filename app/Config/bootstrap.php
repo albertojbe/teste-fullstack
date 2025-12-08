f9f205a0f91e148675441bfc8846c05e4e35c17f
Alberto Cesar <albertocesar.be@gmail.com>
2025-12-08 00:58:12 -0300

    chore: adicionando lib phpspreadsheet ao composer

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -13,6 +13,10 @@
  * @since         CakePHP(tm) v 0.10.8.2117
  */
 
+
+require APP . DS . 'Vendor' . DS . 'autoload.php';
+
+
 // Setup a 'default' cache configuration for use in the application.
 Cache::config('default', array('engine' => 'File'));
 
