fb099d03275627c8d43668062ce5341406e76371
Alberto Cesar <albertocesar.be@gmail.com>
2025-12-08 14:27:14 -0300

    fix: corrige duas mensagens de erro em cadastrar servico

--- app/Controller/ServicosController.php
+++ app/Controller/ServicosController.php
@@ -1,16 +1,29 @@
 <?php
 
-class ServicosController extends AppController {
+class ServicosController extends AppController
+{
     public $uses = ['Servico'];
 
 
-    public function add() {
+    public function add()
+    {
         if ($this->request->is('post')) {
             $this->Servico->create();
-            if ($this->Servico->save($this->request->data)) {
-                $this->Session->setFlash('Serviço cadastrado com sucesso.', 'default', ['class' => 'success-message'], 'success');
+            $this->Servico->set($this->request->data);
+
+            if (!$this->Servico->validates()) {
+                foreach ($this->Servico->validationErrors as $campo => $mensagens) {
+                    foreach ($mensagens as $mensagem) {
+                        $erros[] = "{$mensagem}";
+                    }
+                }
+                $this->Session->setFlash(implode('<br>', $erros), 'default', ['class' => 'error-message'], 'error');
             }
-            $this->Session->setFlash('Erro ao cadastrar serviço. Verifique os dados.', 'default', ['class' => 'error-message'], 'error'); 
+            else if ($this->Servico->save($this->request->data)) {
+                $this->Session->setFlash('Serviço cadastrado com sucesso.', 'default', ['class' => 'success-message'], 'success');
+            } else {
+                $this->Session->setFlash('Erro ao cadastrar serviço. Verifique os dados.', 'default', ['class' => 'error-message'], 'error');
+            }
         }
         
         return $this->redirect($this->referer());
